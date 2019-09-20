f161d42b3c7a3d7a19b5dbe926867f493c3348c3
lisperow <lisperow@yandex.ru>
2019-09-20 21:09:53 +0300

    remove redux

--- src/index.js
+++ src/index.js
@@ -1,26 +1,14 @@
 import React from 'react';
 import { render } from 'react-dom';
-import { Provider } from 'react-redux';
-import thunk from 'redux-thunk';
-import { createStore, applyMiddleware, compose } from 'redux';
 import reducers from './reducers';
 import App from './App';
-import { fetchTasks } from './actions';
 
 /* eslint-disable no-underscore-dangle */
 const ext = window.__REDUX_DEVTOOLS_EXTENSION__;
 const devtoolMiddleware = ext && ext();
 /* eslint-enable */
 
-const store = createStore(
-  reducers,
-  compose(
-    applyMiddleware(thunk),
-    devtoolMiddleware,
-  ),
-);
-
-store.dispatch(fetchTasks());
+const store = createStore();
 
 render(
   <Provider store={store}>
