a3d3752bf187dd7072e7474a3dfff02df2d82a1a
lisperow <lisperow@yandex.ru>
2019-09-27 21:02:57 +0300

    add redux-devtools

--- src/store/index.js
+++ src/store/index.js
@@ -1,4 +1,7 @@
 import createStore from 'storeon';
 import currencies from './currencies.js';
 
-export default createStore([currencies]);
+export default createStore([
+  currencies,
+  process.env.NODE_ENV !== 'production' && require('storeon/devtools')
+]);
