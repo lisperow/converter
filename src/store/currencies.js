d9f9d1932596f5c7b6a27dd0dd0a6bc6cd6c9ea4
lisperow <lisperow@yandex.ru>
2019-09-29 01:43:51 +0300

    save

--- src/store/currencies.js
+++ src/store/currencies.js
@@ -1,15 +1,10 @@
-import axios from 'axios';
 import routes from '../routes';
+
+const data = fetch(routes.ratesUrl())
+  .then(response => response.json());
 
 export default store => {
   store.on('@init', () => {
-    const curr = axios.get(routes.ratesUrl())
-      .then(response => response.data)
-      .catch(e => console.log(e));
-    return { currencies: curr };
-  });
-
-  store.on('currencies/save', ({ currencies }, currencie) => {
-    return { currencies: currencies.concat([currencie]) }
+    return { currencies: [data] };
   });
 }
